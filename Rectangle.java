7b9dd96d8d5d31efd380d94ddf4c33efaaa144f6
git-shru <mca.1904@unigoa.ac.in>
2021-05-31 17:33:16 +0530

    fix files

--- Rectangle.java
+++ Rectangle.java
@@ -5,6 +5,22 @@ public class Rectangle  implements Shapes{
 	int length = 12;
 	int width = 10;
 	
+	public int getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
 	public void calculateArea() {
 		double area = length*width;
 		System.out.println("area of rectangle = " + area);
--- Square.java
+++ Square.java
@@ -4,6 +4,14 @@ public class Square implements Shapes{
 	
 	int side = 4;
 	
+	public int getSide() {
+		return side;
+	}
+
+	public void setSide(int side) {
+		this.side = side;
+	}
+
 	public void calculateArea() {
 		double area = side*side;
 		System.out.println("area of square = " + area);
