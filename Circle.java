3f7ce56166d77902b9ed12da3e2e2350da105bf2
git-shru <mca.1904@unigoa.ac.in>
2021-05-31 17:34:02 +0530

    fix files

--- Circle.java
+++ Circle.java
@@ -4,6 +4,14 @@ public class Circle  implements Shapes{
 	
 	int radius = 15;
 	
+	public int getRadius() {
+		return radius;
+	}
+
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+
 	public void calculateArea() {
 		double area = 3.14*radius*radius;
 		System.out.println("area of circle = " + area);
